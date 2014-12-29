e18caef99296d541696f014a5be16ed091ca5fee
Omar Estrella <omarestrella@bitcreative.net>
2014-12-29 15:58:08 -0500

    Storyboarding the shots collection view

--- dribbble/ProfileViewController.h
+++ dribbble/ProfileViewController.h
@@ -17,4 +17,6 @@
 @property (weak, nonatomic) IBOutlet UIImageView *imageView;
 @property (weak, nonatomic) IBOutlet UILabel *name;
 
+@property (weak, nonatomic) IBOutlet UIView *shotsContainer;
+
 @end
